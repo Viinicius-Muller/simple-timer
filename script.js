4b394908c0a0e4a491eeb48ca74408a586f0a882
Viinicius-Muller <zandreviniciusmuller@gmail.com>
2025-03-08 11:17:51 -0300

    Simple website design and timer prototype

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 'use strict'
 
-console.log('working')
+const form = document.querySelector('form')
+const button = document.querySelector('button')
+const minutes = document.querySelector('.minutes')
+const seconds = document.querySelector('.seconds')
+const showTime = document.querySelector('.showTime')
+
+let newTimer;
+
+
+let totalSeconds;
+let totalMs;
+
+button.addEventListener('click', function(e) {
+    clearInterval(newTimer);
+    e.preventDefault();
+    totalSeconds = +minutes.value * 60 + +seconds.value;
+    totalMs = +minutes.value * 60 * 1000 + +seconds.value * 1000;
+    console.log(totalSeconds, totalMs);
+
+    newTimer = setInterval(() => {
+        if (totalSeconds === 0) {
+            showTime.textContent = `${String(Math.trunc(totalSeconds / 60)).padStart(2,0)}:${String(totalSeconds % 60).padStart(2,0)}`
+            clearInterval(newTimer)
+        }
+        showTime.textContent = `${String(Math.trunc(totalSeconds / 60)).padStart(2,0)}:${String(totalSeconds % 60).padStart(2,0)}`;
+        totalSeconds -= 1;
+    }, 1000)
+})
+
