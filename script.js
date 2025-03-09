6a94976de3d30d5bd55fd42e69c17a32fac07f8d
Viinicius-Muller <zandreviniciusmuller@gmail.com>
2025-03-09 11:46:37 -0300

    Added date time based timer.

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 'use strict'
 
-const form = document.querySelector('form')
-const start = document.querySelector('.start')
-const alternate = document.querySelector('.alternate')
-const minutes = document.querySelector('.minutes')
-const seconds = document.querySelector('.seconds')
-const showTime = document.querySelector('.showTime')
-const pop = new Audio('./pop.mp3')
+const regular = document.getElementById('regular');
+const timeBased = document.getElementById('timeBased');
+const regularTimerDiv = document.querySelector('.timer--regular')
+const hourTimerDiv = document.querySelector('.timer--date')
 
+//  Regular Timer
+const start = document.querySelector('.start');
+const alternate = document.querySelector('.alternate');
+const minutes = document.querySelector('.minutes');
+const seconds = document.querySelector('.seconds');
+const showTime = document.querySelector('.showTime');
+const pop = new Audio('./pop.mp3');
+
+//  Date Hour Timer
+const timeForm = document.getElementById('time--form');
+const timeInput = document.getElementById('time');
+const timer2 = document.querySelector('.timer--2');
 
 
 let newTimer;
@@ -24,7 +33,7 @@ start.addEventListener('click', function(e) {
         clearInterval(newTimer);
         seconds.value = String(totalSeconds % 60).padStart(2,0);
         minutes.value = String(Math.trunc(totalSeconds / 60)).padStart(2,0);
-        start.textContent = 'Resume'
+        start.textContent = 'Resume';
         state = false;
         return;
     }
@@ -55,12 +64,80 @@ start.addEventListener('click', function(e) {
 
 function stopTimer(e){
     e.preventDefault();
-    state = false;
     clearInterval(newTimer);
+    
+    state = false;
     start.textContent = 'Start';
     seconds.value = '00'
     minutes.value = '00'
     showTime.textContent = '00:00';
+    totalSeconds = 0;
 }
 
 alternate.addEventListener('click', stopTimer)
+
+//  timeForm
+timeForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    if (+timeInput.value == false) return;
+    
+    clearInterval(newTimer);
+    const newArr = timeInput.value.split(':');
+    const [hour,min] = newArr
+
+    totalSeconds = 0;
+    totalSeconds = (hour * 60 * 60) + (min * 60);
+
+    const newDate = new Date();
+    const dateSeconds = (newDate.getHours() * 60 * 60 + newDate.getMinutes() * 60 + newDate.getSeconds())
+
+    totalSeconds = totalSeconds > dateSeconds ? totalSeconds - dateSeconds : (24 * 60 * 60 - dateSeconds + totalSeconds);
+
+    const str = `${String(Math.trunc(totalSeconds / 60 / 60)).padStart(2,0)}:${String(Math.trunc(totalSeconds / 60 % 60)).padStart(2,0)}:${String(totalSeconds % 60).padStart(2,0)}`
+    timer2.textContent = str;
+
+    //  Start timer
+    newTimer = setInterval(() => {
+        totalSeconds -= 1;
+        if (totalSeconds === 0) {
+            //Reset config
+            state = false;
+            timer2.textContent = '00:00';
+            pop.play();
+            clearInterval(newTimer);
+        }
+        timer2.textContent = `${String(Math.trunc(totalSeconds / 60 / 60)).padStart(2,0)}:${String(Math.trunc(totalSeconds / 60 % 60)).padStart(2,0)}:${String(totalSeconds % 60).padStart(2,0)}`;
+    }, 1000);
+})
+
+function resetConfig() {
+    clearInterval(newTimer);
+    state = false;
+    totalSeconds = 0;
+
+    //  Reset first timer
+    start.textContent = 'Start';
+    seconds.value = '00'
+    minutes.value = '00'
+    showTime.textContent = '00:00';
+
+    //  Reset second timer
+    timer2.textContent = '00:00';
+    timeInput.value = '00:00'
+}
+
+regular.addEventListener('click', function() {
+    resetConfig();
+    regularTimerDiv.classList.remove('hidden');
+    hourTimerDiv.classList.add('hidden')
+})
+
+timeBased.addEventListener('click', function() {
+    resetConfig();
+    regularTimerDiv.classList.add('hidden');
+    hourTimerDiv.classList.remove('hidden')
+})
+
+
+//  background-color: #3AA3A0; | background-color: #FD49A0;
