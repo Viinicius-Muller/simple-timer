160facf457c230370b3b2649bf529529a4065291
Viinicius-Muller <zandreviniciusmuller@gmail.com>
2025-03-10 11:16:43 -0300

    Update functions to private functions by using ES6 Classes.

--- script.js
+++ script.js
@@ -1,143 +1,180 @@
-'use strict'
+"use strict";
 
-const regular = document.getElementById('regular');
-const timeBased = document.getElementById('timeBased');
-const regularTimerDiv = document.querySelector('.timer--regular')
-const hourTimerDiv = document.querySelector('.timer--date')
+//  Base DOM Elements
+const regular = document.getElementById("regular");
+const timeBased = document.getElementById("timeBased");
+const regularTimerDiv = document.querySelector(".timer--regular");
+const hourTimerDiv = document.querySelector(".timer--date");
 
 //  Regular Timer
-const start = document.querySelector('.start');
-const alternate = document.querySelector('.alternate');
-const minutes = document.querySelector('.minutes');
-const seconds = document.querySelector('.seconds');
-const showTime = document.querySelector('.showTime');
-const pop = new Audio('./pop.mp3');
+const start = document.querySelector(".start");
+const stopB = document.querySelector(".stop");
+const minutes = document.querySelector(".minutes");
+const seconds = document.querySelector(".seconds");
+const showTime = document.querySelector(".showTime");
+const pop = new Audio("./pop.mp3");
 
 //  Date Hour Timer
-const timeForm = document.getElementById('time--form');
-const timeInput = document.getElementById('time');
-const timer2 = document.querySelector('.timer--2');
+const timeForm = document.getElementById("time--form");
+const timeInput = document.getElementById("time");
+const timer2 = document.querySelector(".timer--2");
+const submitButton = document.querySelector(".submit-btton");
 
-
+//  Initial Values
 let newTimer;
 let totalSeconds;
 let state = false;
 
-start.addEventListener('click', function(e) {
-    e.preventDefault();
-    //  Check value
-    if (isNaN(+minutes.value) || isNaN(+seconds.value)) return;
+class App {
+  constructor() {
+    regular.addEventListener("click", this._swapToRegular.bind(this));
+    timeBased.addEventListener("click", this._swapToDate.bind(this));
+  }
 
-    //  Pause
-    if(state) {
-        clearInterval(newTimer);
-        seconds.value = String(totalSeconds % 60).padStart(2,0);
-        minutes.value = String(Math.trunc(totalSeconds / 60)).padStart(2,0);
-        start.textContent = 'Resume';
-        state = false;
-        return;
-    }
-
-    //  Resume / Reset
-    clearInterval(newTimer);
-    totalSeconds = +minutes.value * 60 + +seconds.value;
-    if (totalSeconds <= 0) return;
-    start.textContent = 'Pause'
-    showTime.textContent = `${String(Math.trunc(totalSeconds / 60)).padStart(2,0)}:${String(totalSeconds % 60).padStart(2,0)}`
-    state = true;
-
-    //  Start timer
-    newTimer = setInterval(() => {
-        totalSeconds -= 1;
-        if (totalSeconds === 0) {
-            //Reset config
-            state = false;
-            showTime.textContent = '00:00';
-            start.textContent = 'Start';
-            pop.play();
-            clearInterval(newTimer);
-        }
-        showTime.textContent = `${String(Math.trunc(totalSeconds / 60)).padStart(2,0)}:${String(totalSeconds % 60).padStart(2,0)}`;
-    }, 1000)
-})
-
-
-function stopTimer(e){
-    e.preventDefault();
-    clearInterval(newTimer);
-    
-    state = false;
-    start.textContent = 'Start';
-    seconds.value = '00'
-    minutes.value = '00'
-    showTime.textContent = '00:00';
-    totalSeconds = 0;
-}
-
-alternate.addEventListener('click', stopTimer)
-
-//  timeForm
-timeForm.addEventListener('submit', function(e) {
-    e.preventDefault();
-    
-    if (+timeInput.value == false) return;
-    
-    clearInterval(newTimer);
-    const newArr = timeInput.value.split(':');
-    const [hour,min] = newArr
-
-    totalSeconds = 0;
-    totalSeconds = (hour * 60 * 60) + (min * 60);
-
-    const newDate = new Date();
-    const dateSeconds = (newDate.getHours() * 60 * 60 + newDate.getMinutes() * 60 + newDate.getSeconds())
-
-    totalSeconds = totalSeconds > dateSeconds ? totalSeconds - dateSeconds : (24 * 60 * 60 - dateSeconds + totalSeconds);
-
-    const str = `${String(Math.trunc(totalSeconds / 60 / 60)).padStart(2,0)}:${String(Math.trunc(totalSeconds / 60 % 60)).padStart(2,0)}:${String(totalSeconds % 60).padStart(2,0)}`
-    timer2.textContent = str;
-
-    //  Start timer
-    newTimer = setInterval(() => {
-        totalSeconds -= 1;
-        if (totalSeconds === 0) {
-            //Reset config
-            state = false;
-            timer2.textContent = '00:00';
-            pop.play();
-            clearInterval(newTimer);
-        }
-        timer2.textContent = `${String(Math.trunc(totalSeconds / 60 / 60)).padStart(2,0)}:${String(Math.trunc(totalSeconds / 60 % 60)).padStart(2,0)}:${String(totalSeconds % 60).padStart(2,0)}`;
-    }, 1000);
-})
-
-function resetConfig() {
+  // Both timers reset
+  _resetConfig() {
     clearInterval(newTimer);
     state = false;
     totalSeconds = 0;
 
     //  Reset first timer
-    start.textContent = 'Start';
-    seconds.value = '00'
-    minutes.value = '00'
-    showTime.textContent = '00:00';
+    start.textContent = "Start";
+    seconds.value = "00";
+    minutes.value = "00";
+    showTime.textContent = "00:00";
 
     //  Reset second timer
-    timer2.textContent = '00:00';
-    timeInput.value = '00:00'
+    submitButton.value = "Start";
+    submitButton.style.background = "#3AA3A0";
+    timer2.textContent = "00:00";
+    timeInput.value = "00:00";
+  }
+
+  _swapToRegular() {
+    this._resetConfig();
+    regularTimerDiv.classList.remove("hidden");
+    hourTimerDiv.classList.add("hidden");
+  }
+
+  _swapToDate() {
+    this._resetConfig();
+    regularTimerDiv.classList.add("hidden");
+    hourTimerDiv.classList.remove("hidden");
+  }
 }
 
-regular.addEventListener('click', function() {
-    resetConfig();
-    regularTimerDiv.classList.remove('hidden');
-    hourTimerDiv.classList.add('hidden')
-})
+class RegularTimer extends App {
+  constructor() {
+    super();
+    start.addEventListener("click", this._startTimer.bind(this));
+    stopB.addEventListener("click", this._resetConfig);
+  }
 
-timeBased.addEventListener('click', function() {
-    resetConfig();
-    regularTimerDiv.classList.add('hidden');
-    hourTimerDiv.classList.remove('hidden')
-})
+  _startTimer() {
+    //  Check value
+    if (isNaN(+minutes.value) || isNaN(+seconds.value)) return;
 
+    //  Pause
+    if (state) {
+      clearInterval(newTimer);
+      seconds.value = String(totalSeconds % 60).padStart(2, 0);
+      minutes.value = String(Math.trunc(totalSeconds / 60)).padStart(2, 0);
+      start.textContent = "Resume";
+      state = false;
+      return;
+    }
 
-//  background-color: #3AA3A0; | background-color: #FD49A0;
+    //  Resume / Reset
+    clearInterval(newTimer);
+
+    totalSeconds = +minutes.value * 60 + +seconds.value;
+    if (totalSeconds <= 0) return;
+
+    start.textContent = "Pause";
+    showTime.textContent = `${String(Math.trunc(totalSeconds / 60)).padStart(
+      2,
+      0
+    )}:${String(totalSeconds % 60).padStart(2, 0)}`;
+    state = true;
+
+    //  Start timer
+    newTimer = setInterval(() => {
+      totalSeconds -= 1;
+      if (totalSeconds === 0) {
+        //  Reset config
+        this._resetConfig();
+        pop.play();
+      }
+      showTime.textContent = `${String(Math.trunc(totalSeconds / 60)).padStart(
+        2,
+        0
+      )}:${String(totalSeconds % 60).padStart(2, 0)}`;
+    }, 1000);
+  }
+}
+
+class DateTimer extends App {
+  constructor() {
+    super();
+    timeForm.addEventListener("submit", this._startTimer.bind(this));
+  }
+
+  _startTimer(e) {
+    e.preventDefault();
+
+    if (+timeInput.value == false) return;
+    if (state === true) {
+      this._resetConfig();
+      return;
+    }
+
+    state = true;
+    submitButton.value = "Stop";
+    submitButton.style.background = "#FD49A0";
+
+    clearInterval(newTimer);
+    const [hour, min] = timeInput.value.split(":");
+
+    totalSeconds = hour * 60 * 60 + min * 60;
+
+    const newDate = new Date();
+    const dateSeconds =
+      newDate.getHours() * 60 * 60 +
+      newDate.getMinutes() * 60 +
+      newDate.getSeconds();
+
+    // Check if date is today or tomorrow
+    totalSeconds =
+      totalSeconds > dateSeconds
+        ? totalSeconds - dateSeconds
+        : 24 * 60 * 60 - dateSeconds + totalSeconds;
+
+    const str = `${String(Math.trunc(totalSeconds / 60 / 60)).padStart(
+      2,
+      0
+    )}:${String(Math.trunc((totalSeconds / 60) % 60)).padStart(2, 0)}:${String(
+      totalSeconds % 60
+    ).padStart(2, 0)}`;
+    timer2.textContent = str;
+
+    //  Start timer
+    newTimer = setInterval(() => {
+      totalSeconds -= 1;
+      if (totalSeconds === 0) {
+        //Reset config
+        this._resetConfig();
+        pop.play();
+      }
+      timer2.textContent = `${String(
+        Math.trunc(totalSeconds / 60 / 60)
+      ).padStart(2, 0)}:${String(Math.trunc((totalSeconds / 60) % 60)).padStart(
+        2,
+        0
+      )}:${String(totalSeconds % 60).padStart(2, 0)}`;
+    }, 1000);
+  }
+}
+
+const app = new App();
+const regularTimer = new RegularTimer();
+const dateTimer = new DateTimer();
